c170c1efaa481d3e08408ad3022cde4c791e1778
David Steele <dsteele@gmail.com>
2013-12-27 13:49:51 -0500

    Create data update script for the web page

---
+++ update.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+cp ../cloudprint*deb deb/
+
+cd deb
+echo MD5Sum >checksums.txt
+md5sum *deb >>checksums.txt
+echo "\nSHA1" >>checksums.txt
+sha1sum *deb >>checksums.txt
+echo "\nSHA256" >>checksums.txt
+sha256sum *deb >>checksums.txt
+echo  >>checksums.txt
+
+gpg --clearsign checksums.txt
+rm checksums.txt
+mv checksums.txt.asc checksums.txt
+
+cd ..
+
+cd man
+./makeman
+cd ..
+
+cd repo
+./clearit
+./addit
+cd ..
+
+
+
